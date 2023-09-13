83240ba28542b396c7c86b6f501f0348b3a8fb8a
Jared Parsons <jared@popcornbear.org>
2023-09-13 16:57:04 -0700

    now that I built

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import { exec } from '@actions/exec';
+import * as core from '@actions/core'
+import { exec } from '@actions/exec'
 import * as artifact from '@actions/artifact'
 import * as os from 'os'
 import * as path from 'path'
@@ -10,24 +10,31 @@ import * as path from 'path'
  */
 export async function run(): Promise<void> {
   try {
+    console.log('Here')
     // install compiler log
-    let installArgs = ['tool', 'install', '--global', 'Basic.CompilerLog', '--add-source', 'https://api.nuget.org/v3/index.json'];
+    let installArgs = [
+      'tool',
+      'install',
+      '--global',
+      'Basic.CompilerLog',
+      '--add-source',
+      'https://api.nuget.org/v3/index.json'
+    ]
 
-    console.log("Installing Basic.CompilerLog");
-    let exitCode = await exec('dotnet', installArgs, { ignoreReturnCode: true });
+    console.log('Installing Basic.CompilerLog')
+    let exitCode = await exec('dotnet', installArgs, { ignoreReturnCode: true })
     if (exitCode > 1) {
-      throw new Error("dotnet tool install failed.");
+      throw new Error('dotnet tool install failed.')
     }
 
     // add .dotnet/tools to the path
-    core.addPath(path.join(os.homedir(), '.dotnet', 'tools'));
+    core.addPath(path.join(os.homedir(), '.dotnet', 'tools'))
 
     await exec('complog', ['create', '--out', 'build.complog'])
 
-
-    console.log("Publishing the artifact");
-    let client = artifact.create();
-    await client.uploadArtifact("build.complog", ['build.complog'], '.');
+    console.log('Publishing the artifact')
+    let client = artifact.create()
+    await client.uploadArtifact('build.complog', ['build.complog'], '.')
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
