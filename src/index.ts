2b38de2a5ddf769c401a7689a47dabb41bf0e3fc
Jared Parsons <jared@paranoidcoding.org>
2023-09-13 18:08:48 -0700

    Initial workflow (#1)
    Initial workflow changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { exec } from '@actions/exec'
+import * as artifact from '@actions/artifact'
+import * as os from 'os'
+import * as path from 'path'
 
 /**
  * The main function for the action.
@@ -7,22 +10,71 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    await installCompilerLog()
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // add .dotnet/tools to the path
+    core.addPath(path.join(os.homedir(), '.dotnet', 'tools'))
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const complogFile = await runCompilerLog()
+    await publishCompilerLog(complogFile)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+async function installCompilerLog(): Promise<void> {
+  // install compiler log
+  const installArgs = [
+    'tool',
+    'install',
+    '--global',
+    'Basic.CompilerLog',
+    '--add-source',
+    'https://api.nuget.org/v3/index.json'
+  ]
+
+  console.log('Installing Basic.CompilerLog')
+  const exitCode = await exec('dotnet', installArgs, {
+    ignoreReturnCode: true
+  })
+
+  if (exitCode > 1) {
+    throw new Error('dotnet tool install failed.')
+  }
+}
+
+async function runCompilerLog(): Promise<string> {
+  const args = ['create', '--out']
+
+  let output = core.getInput('complog')
+  if (isNullOrEmpty(output)) {
+    output = 'build.complog'
+  }
+
+  args.push(output)
+
+  const binlog = core.getInput('binlog')
+  if (!isNullOrEmpty(binlog)) {
+    args.push(binlog)
+  }
+
+  await exec('complog', args)
+  return output
+}
+
+async function publishCompilerLog(complogFile: string): Promise<void> {
+  console.log('Publishing the artifact')
+  let name = core.getInput('artifact')
+  if (isNullOrEmpty(name)) {
+    name = 'build.complog'
+  }
+  const client = artifact.create()
+  await client.uploadArtifact(name, [complogFile], '.')
+}
+
+function isNullOrEmpty(str: string): boolean {
+  return str === null || str === ''
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
